3f8c1e50b77ee131e713b530a0e79d89c5b65df7
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-10 08:22:00 +0300

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <section className="container flex flex-col items-center justify-center min-h-screen">
+      <Button>Click on Me</Button>
+    </section>
+  );
+}
