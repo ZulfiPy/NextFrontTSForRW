1667c492b3039841e73e049e7861a354641c7968
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-12 09:50:54 +0300

    Sign In page created

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SignInForm from "@/components/SignInForm";
+
+const SignInPage = () => {
+    return (
+        <section className="container flex flex-col items-center justify-center min-h-screen">
+            <SignInForm />
+        </section>
+    )
+}
+
+export default SignInPage;
