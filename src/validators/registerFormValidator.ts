429c919a7ec3f720d5b47455b225392fcf8ed09a
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-13 13:26:49 +0300

    refactor register form validator using .refine()

--- src/validators/registerFormValidator.ts
+++ src/validators/registerFormValidator.ts
@@ -3,26 +3,41 @@ import { z } from "zod";
 const currentYear: number = new Date().getFullYear();
 const eighteenYearsAgo: number = currentYear - 18;
 
+const firstNameSchema = z.string()
+    .refine((value) => 3 <= value.length && value.length <= 255, {
+        message: "First name must be at least between 3 and 255 characters."
+    });
+
+const lastNameSchema = z.string()
+    .refine((value) => 3 <= value.length && value.length <= 255, {
+        message: "Last name must be at least between 3 and 255 characters."
+    });
+
+const isikukoodSchema = z.string()
+    .refine((code) => code.length === 11, {
+        message: "The ID code must consist of 11 digits."
+    });
+
 const daySchema = z.number()
-    .refine(day => day >= 1 && day <= 31, {
+    .refine((day) => day >= 1 && day <= 31, {
         message: "Day of your birth must be between 1 and 31."
     });
 
 const monthSchema = z.number()
-    .refine(month => month >= 1 && month <= 12, {
+    .refine((month) => month >= 1 && month <= 12, {
         message: "Month of your birth must be between 1 and 12."
     });
 
 const yearSchema = z.number()
-    .refine(year => year >= 1960 && year <= eighteenYearsAgo , {
+    .refine((year) => year >= 1960 && year <= eighteenYearsAgo, {
         message: `Year of your birth must be between 1960 and ${eighteenYearsAgo}`
     });
 
 export const registerSchema = z.object({
-    firstName: z.string().min(3).max(255),
-    lastName: z.string().min(3).max(255),
+    firstName: firstNameSchema,
+    lastName: lastNameSchema,
     email: z.string().email(),
-    isikukood: z.string().min(11).max(11),
+    isikukood: isikukoodSchema,
     day: daySchema,
     month: monthSchema,
     year: yearSchema,
