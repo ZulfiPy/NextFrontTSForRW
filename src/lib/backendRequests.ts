f84ea36194566bfd1c7fff019ebf5a283622a740
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-17 08:41:04 +0300

    sinInUserRequest modified return and wrapped into the try and catch block

--- src/lib/backendRequests.ts
+++ src/lib/backendRequests.ts
@@ -11,18 +11,22 @@ async function registerUserRequest(userData: RegisterUserData) {
 };
 
 async function signInUserRequest(userDataForSignIn: { username: String, password: String }) {
-    const response = await fetch(`http://localhost:3500/auth`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        credentials: "include",
-        body: JSON.stringify(userDataForSignIn)
-    });
+    try {
+        const response = await fetch(`http://localhost:3500/auth`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include",
+            body: JSON.stringify(userDataForSignIn)
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    return data;
+        return { data, status: response.status };
+    } catch (error) {
+        return { data: error, status: 403 };
+    }
 }
 
 export {
