cac3f9ba5690a34041a5ba5cb3ce598969474381
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-18 08:36:20 +0300

    use context of AuthProvider context and when successfully logged it, set a new auth data

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -3,6 +3,9 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthProvider";
 import Link from "next/link";
 import {
     Form,
@@ -11,7 +14,6 @@ import {
     FormField,
     FormItem,
     FormLabel,
-    FormMessage
 } from "@/components/ui/form";
 import {
     Card,
@@ -23,9 +25,12 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+import { toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+
+import { signInUserRequest } from "@/lib/backendRequests";
 import { loginSchema } from "@/validators/loginFormValidator";
 type loginInputType = z.infer<typeof loginSchema>
-
 
 const SignInForm = () => {
     const form = useForm<loginInputType>({
@@ -35,9 +40,20 @@ const SignInForm = () => {
             password: ""
         }
     });
+    const router = useRouter();
+    const { setAuth } = useContext(AuthContext);
 
-    function handleLoginForm(values: loginInputType) {
-        console.log(values)
+    async function handleLoginForm(values: loginInputType) {
+        const signInResponse = await signInUserRequest(values);
+
+        if (signInResponse.status === 200 && signInResponse?.data?.jwt) {
+            setAuth({ username: values.username, accessToken: signInResponse.data.jwt });
+            toast.success('You have been successfully logged in!')
+            form.reset();
+            router.push('/');
+        } else if (signInResponse.status === 403) {
+            toast.error('Some error occured, please try again!')
+        }
     }
 
     return (
@@ -88,11 +104,11 @@ const SignInForm = () => {
                                 )}
                             />
 
-                            <Button type="submit">Sign In</Button>
+                            <Button type="submit" className="font-bold">Sign In</Button>
 
                             <Link
                                 href="/register"
-                                className="self-center hover:underline">
+                                className="self-center underline">
                                 Forgot username or password?
                             </Link>
                         </form>
