0fc8aac0271fd6327d4e975fa134cdc8ecfd9722
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-20 08:04:30 +0300

    AuthContextType edites, roles array consisted of numbers added

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -5,14 +5,16 @@ import { useState, createContext } from "react";
 interface AuthContextType {
     auth: {
         username: string,
+        roles: number[],
         accessToken: string
     },
-    setAuth: React.Dispatch<React.SetStateAction<{ username: string, accessToken: string }>>
+    setAuth: React.Dispatch<React.SetStateAction<{ username: string, roles: number[], accessToken: string }>>
 }
 
 const defaultAuthValues: AuthContextType = {
     auth: {
         username: '',
+        roles: [],
         accessToken: ''
     },
     setAuth: () => { }
@@ -21,7 +23,7 @@ const defaultAuthValues: AuthContextType = {
 const AuthContext = createContext<AuthContextType>(defaultAuthValues);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }): JSX.Element => {
-    const [auth, setAuth] = useState<{ username: string, accessToken: string }>({ username: '', accessToken: '' });
+    const [auth, setAuth] = useState<{ username: string, roles: number[], accessToken: string }>({ username: '', roles: [], accessToken: '' });
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
