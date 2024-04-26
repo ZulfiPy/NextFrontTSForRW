018ec983f1448dd94b2f92a1359351d78f9f3b34
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-26 07:38:30 +0300

    /new-task page created

---
+++ src/app/components/tasks/new-task/page.tsx
@@ -0,0 +1,12 @@
+import NewTaskForm from "@/components/NewTaskForm";
+
+const NewTaskPage = () => {
+    return (
+        <section className="container flex flex-col items-center justify-center min-h-screen space-y-10">
+            <h1 className="text-3xl font-bold mt-10">New Task Form</h1>
+            <NewTaskForm />
+        </section>
+    )
+}
+
+export default NewTaskPage;
