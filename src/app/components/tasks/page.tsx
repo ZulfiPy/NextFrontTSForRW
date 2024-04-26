b78dbb3222b261de81cdcb19ca7d1dfc63cfec74
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-26 07:37:58 +0300

    button config. with router

--- src/app/components/tasks/page.tsx
+++ src/app/components/tasks/page.tsx
@@ -1,14 +1,18 @@
-'use-client';
+'use client';
 import TasksTable from "@/components/TasksTable";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const TasksPage = () => {
+    const router = useRouter();
+
     return (
         <section className="container flex flex-col items-center justify-center min-h-screen space-y-11">
             <h1 className="text-3xl font-bold mt-10">Tasks Table</h1>
             <TasksTable />
             <Button
-                className="py-8 shadow-2xl font-bold text-md">
+                className="py-8 shadow-2xl font-bold text-md"
+                onClick={() => router.push('tasks/new-task')}>
                 Add a New Task
             </Button>
         </section>
