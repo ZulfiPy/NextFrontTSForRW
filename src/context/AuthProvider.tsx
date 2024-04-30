db49ff339c67816fe8ba32534649682b99c26277
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-04-30 09:13:13 +0300

    persist state added to Auth Context

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
 interface AuthContextType {
     auth: {
@@ -8,7 +8,9 @@ interface AuthContextType {
         roles: number[],
         accessToken: string
     },
-    setAuth: React.Dispatch<React.SetStateAction<{ username: string, roles: number[], accessToken: string }>>
+    setAuth: React.Dispatch<React.SetStateAction<{ username: string, roles: number[], accessToken: string }>>,
+    persist: boolean,
+    setPersist: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const defaultAuthValues: AuthContextType = {
@@ -17,16 +19,19 @@ const defaultAuthValues: AuthContextType = {
         roles: [],
         accessToken: ''
     },
-    setAuth: () => { }
+    setAuth: () => { },
+    persist: false,
+    setPersist: () => { }
 }
 
 const AuthContext = createContext<AuthContextType>(defaultAuthValues);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }): JSX.Element => {
     const [auth, setAuth] = useState<{ username: string, roles: number[], accessToken: string }>({ username: '', roles: [], accessToken: '' });
+    const [persist, setPersist] = useState<boolean>(false);
 
     return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
+        <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
             {children}
         </AuthContext.Provider>
     )
