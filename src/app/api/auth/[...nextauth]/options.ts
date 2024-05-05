54a8b6daade5515ad7ac97ca687a265c60f4360d
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-05 09:54:43 +0300

    providers moved to sepparate file

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,48 +1,10 @@
-import CredentialsProvider from "next-auth/providers/credentials";
-import connectDB from "@/config/MongoDbConn";
-import User from "@/models/User";
-import bcrypt from "bcryptjs";
 import { JWT } from "next-auth/jwt";
 import jwt from "jsonwebtoken";
 import NextAuth from "next-auth";
 import { NextApiRequest, NextApiResponse } from "next";
+import { providers } from "./providers";
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-    const providers = [
-        CredentialsProvider({
-            credentials: {
-                username: {
-                    label: 'Username',
-                    type: 'text',
-                    placeholder: "Enter a username"
-                },
-                password: {
-                    label: 'Password',
-                    type: 'password',
-                    placeholder: 'Enter your COOL password'
-                }
-            },
-            async authorize(credentials, req) {
-                if (!credentials) return null;
-                const { username, password } = credentials;
-                try {
-                    await connectDB();
-                    const user = await User.findOne({ username });
-
-                    if (!user) return null;
-
-                    const comparedPwd = await bcrypt.compare(password, user.password);
-                    if (!comparedPwd) return null;
-
-                    return user;
-                } catch (error) {
-                    console.log('error occurred while logging in', error);
-                    return null;
-                }
-            }
-        })
-    ]
-
     return await NextAuth(req, res, {
         providers,
         callbacks: {
