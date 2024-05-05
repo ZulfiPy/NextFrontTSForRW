d9d15f2daf3aa5da89ec9a869f4ae56ef91a7aeb
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-05 10:13:05 +0300

    customers, fleet and insurance pages initialized

---
+++ src/app/components/customers/page.tsx
@@ -0,0 +1,9 @@
+const CustomerPage = () => {
+    return (
+        <section className="container flex flex-col items-center justify-center min-h-screen">
+            MyCustomer component page
+        </section>
+    )
+}
+
+export default CustomerPage;
---
+++ src/app/components/fleet/page.tsx
@@ -0,0 +1,9 @@
+const FleetPage = () => {
+    return (
+        <section className="container flex flex-col items-center justify-center min-h-screen">
+            MyFleet component page
+        </section>
+    )
+}
+
+export default FleetPage;
