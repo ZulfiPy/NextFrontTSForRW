2648b496f904c37776958d3b504dd9f6c02c4769
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-05 09:52:57 +0300

    middleware checks if session token persists in cookies

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+
+export async function middleware(request: NextRequest) {
+    const cookieStore = cookies();
+    const sessionToken = cookieStore.get('next-auth.session-token')?.value;
+
+    if (!sessionToken) {
+        return NextResponse.redirect(new URL('/register', request.url));
+    }
+}
+
+export const config = {
+    matcher: ['/components/:path*', '/training/:path*']
+}
