e670d952a7bd4e91ee32476b46578596543f7f7f
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-05 09:53:52 +0300

    homepage delivered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
-import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/optionsForServerSide";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <section className="container flex flex-col items-center justify-center min-h-screen">
-      <Button>Click on Me</Button>
+      {session?.user ? (
+        <>
+          Welcome, {session?.user.username}!
+          <Link href='components'
+            className="bg-blue-700 p-2 rounded-lg mt-2"
+          >
+            Check the Components
+          </Link>
+        </>
+      ) : (
+        <>
+          <span>
+            If you are not registered, you can
+            <Link
+              href='register'
+              className="bg-blue-700 p-2 rounded-lg mt-2 inline-block mx-1"
+            >
+              Register
+            </Link>.
+          </span>
+          <span>
+            <Link
+              href='sign-in'
+              className="bg-blue-700 p-2 rounded-lg mt-2 inline-block mx-1">
+              Sign in
+            </Link>
+            if you have an account .
+          </span>
+        </>
+      )}
     </section>
   );
 }
