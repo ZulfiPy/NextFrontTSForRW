405281565e804c1b859d82ca62ac6be693d45389
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-08 08:35:56 +0300

    customer by id view page delivered

---
+++ src/app/components/customers/[id]/view/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+import { useState, useEffect } from "react";
+import Spinner from "@/components/Spinner";
+import ViewCustomerCard from "@/components/ViewCustomerCard";
+import { getCustomerById } from "@/lib/backendRequests";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+const ViewCustomerPage = ({ params }: { params: { id: string } }) => {
+    const { id } = params;
+    const [customer, setCustomer] = useState<Customer>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    useEffect(() => {
+        let isMounted = true;
+
+        async function fetchCustomer() {
+
+            if (isMounted) {
+                const { data, status, error } = await getCustomerById(id);
+
+                if (status === 200 && data) {
+                    setCustomer(data);
+                }
+
+                if (status === 500 && error) {
+                    setError(error);
+                }
+
+                if (status === 401) {
+                    setError('Unauthorized');
+                }
+
+                setLoading(false);
+            }
+        }
+
+        fetchCustomer()
+
+    }, [id]);
+
+    return (
+        <section className="container flex flex-col items-center justify-center min-h-screen">
+            {loading ?
+                (
+                    <Spinner loading={loading} />
+                ) : (
+                    customer ? (
+                        <ViewCustomerCard customer={customer} id={id} />
+                    ) : (
+                        <p>Error: {error.toString()}</p>
+                    )
+                )}
+            <Button
+                type="button"
+                className="mt-6 font-bold"
+                onClick={() => router.back()}
+            >Go back
+            </Button>
+        </section>
+    )
+}
+
+export default ViewCustomerPage;
