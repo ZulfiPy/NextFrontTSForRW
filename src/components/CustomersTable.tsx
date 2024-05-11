af475d536be2853e563531f41c9986b6dc435bd7
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-11 07:58:16 +0300

    CustomersTable conditional rendering modified

--- src/components/CustomersTable.tsx
+++ src/components/CustomersTable.tsx
@@ -25,11 +25,15 @@ import Spinner from "./Spinner";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { getCustomers } from "@/lib/backendRequests";
+import { deleteCustomer } from "@/lib/backendRequests";
+
+import { toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 
 const CustomersTable = () => {
     const [customers, setCustomers] = useState<Customer[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState('No error');
+    const [error, setError] = useState<string | null>(null);
     const router = useRouter();
 
     useEffect(() => {
@@ -38,7 +42,6 @@ const CustomersTable = () => {
 
             if (isMounted) {
                 const { data, status, error } = await getCustomers();
-
                 if (status === 200 && data) {
                     setCustomers(data);
                 }
@@ -62,98 +65,108 @@ const CustomersTable = () => {
         }
     }, []);
 
-    async function handleCustomerDelete() {
-        // handle it
-        console.log('handle it');
+    async function handleCustomerDelete(id: number) {
+        const ID = id.toString()
+        const response = await deleteCustomer(ID);
+
+        if (response.status === 200) {
+            const updatedCustomers = customers.filter(customer => customer.customer_id !== id);
+            setCustomers(updatedCustomers);
+            toast.success('Customer deleted successfully', { autoClose: 1500 });
+        }
     }
 
     return (
         <div className={cn("text-left border-2 rounded-lg p-4", { "h1/3 overflow-y-auto": customers.length >= 4 })}>
             {loading ?
-                (
-                    <Spinner loading={loading} />
-                ) : (
-                    customers.length >= 1 ? (
-                        <Table>
-                            <TableCaption>A list of customers.</TableCaption>
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>First name</TableHead>
-                                    <TableHead>Last name</TableHead>
-                                    <TableHead>Isikukood</TableHead>
-                                    <TableHead>Resident</TableHead>
-                                    <TableHead>Birth date</TableHead>
-                                    <TableHead>Driver License Number</TableHead>
-                                    <TableHead>Phone number</TableHead>
-                                    <TableHead>Email</TableHead>
-                                    <TableHead>Address</TableHead>
-                                    <TableHead>RUD</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {customers.map((customer, index) => (
-                                    <TableRow key={index}>
-                                        <TableCell>{customer.first_name}</TableCell>
-                                        <TableCell>{customer.last_name}</TableCell>
-                                        <TableCell>{customer.personal_id_number}</TableCell>
-                                        <TableCell>{customer.is_estonian_resident ? "Yes" : "No"}</TableCell>
-                                        <TableCell>{customer.birth_date.split('T')[0]}</TableCell>
-                                        <TableCell>{customer.driver_license_number}</TableCell>
-                                        <TableCell>{customer.phone_number}</TableCell>
-                                        <TableCell>{customer.email}</TableCell>
-                                        <TableCell>{customer.address}</TableCell>
-                                        <TableCell>
-                                            <div className="flex space-x-2">
-                                                <div>
-                                                    <button
-                                                        type="button"
-                                                        onClick={() => router.push(`customers/${customer.customer_id}/view`)}>
-                                                        <Eye />
-                                                    </button>
-                                                </div>
-                                                <div>
-                                                    <button
-                                                        type="button"
-                                                        onClick={() => router.push(`customers/${customer.customer_id}/edit`)}>
-                                                        <SquarePen />
-                                                    </button>
-                                                </div>
-                                                <div>
-                                                    <AlertDialog>
-                                                        <AlertDialogTrigger>
-                                                            <X />
-                                                        </AlertDialogTrigger>
-                                                        <AlertDialogContent>
-                                                            <AlertDialogHeader>
-                                                                <AlertDialogTitle>
-                                                                    Are you absolutely sure?
-                                                                </AlertDialogTitle>
-                                                                <AlertDialogDescription>
-                                                                    This action cannot be undone. Thiw will permanently delete selected customer from the database.
-                                                                </AlertDialogDescription>
-                                                            </AlertDialogHeader>
-                                                            <AlertDialogFooter>
-                                                                <AlertDialogCancel>
-                                                                    Cancel
-                                                                </AlertDialogCancel>
-                                                                <AlertDialogAction onClick={handleCustomerDelete}>
-                                                                    Delete
-                                                                </AlertDialogAction>
-                                                            </AlertDialogFooter>
-                                                        </AlertDialogContent>
-                                                    </AlertDialog>
-                                                </div>
-                                            </div>
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table >
-                    ) : (
-                        <p>{error.toString()}</p>
-                    )
-                )}
-        </div >
+                <Spinner loading />
+                : error ?
+                    <p>{error.toString()}</p>
+                    :
+                    customers.length === 0 ?
+                        (<p>Database is empty.</p>)
+                        :
+                        <>
+                            {customers.length !== 0 && (
+                                <Table>
+                                    <TableCaption>A list of customers.</TableCaption>
+                                    <TableHeader>
+                                        <TableRow>
+                                            <TableHead>First name</TableHead>
+                                            <TableHead>Last name</TableHead>
+                                            <TableHead>Isikukood</TableHead>
+                                            <TableHead>Resident</TableHead>
+                                            <TableHead>Birth date</TableHead>
+                                            <TableHead>Driver License Number</TableHead>
+                                            <TableHead>Phone number</TableHead>
+                                            <TableHead>Email</TableHead>
+                                            <TableHead>Address</TableHead>
+                                            <TableHead>RUD</TableHead>
+                                        </TableRow>
+                                    </TableHeader>
+                                    <TableBody>
+                                        {customers.map((customer, index) => (
+                                            <TableRow key={index}>
+                                                <TableCell>{customer.first_name}</TableCell>
+                                                <TableCell>{customer.last_name}</TableCell>
+                                                <TableCell>{customer.personal_id_number}</TableCell>
+                                                <TableCell>{customer.is_estonian_resident ? "Yes" : "No"}</TableCell>
+                                                <TableCell>{customer.birth_date.split('T')[0]}</TableCell>
+                                                <TableCell>{customer.driver_license_number}</TableCell>
+                                                <TableCell>{customer.phone_number}</TableCell>
+                                                <TableCell>{customer.email}</TableCell>
+                                                <TableCell>{customer.address}</TableCell>
+                                                <TableCell>
+                                                    <div className="flex space-x-2">
+                                                        <div>
+                                                            <button
+                                                                type="button"
+                                                                onClick={() => router.push(`customers/${customer.customer_id}/view`)}>
+                                                                <Eye />
+                                                            </button>
+                                                        </div>
+                                                        <div>
+                                                            <button
+                                                                type="button"
+                                                                onClick={() => router.push(`customers/${customer.customer_id}/edit`)}>
+                                                                <SquarePen />
+                                                            </button>
+                                                        </div>
+                                                        <div>
+                                                            <AlertDialog>
+                                                                <AlertDialogTrigger>
+                                                                    <X />
+                                                                </AlertDialogTrigger>
+                                                                <AlertDialogContent>
+                                                                    <AlertDialogHeader>
+                                                                        <AlertDialogTitle>
+                                                                            Are you absolutely sure?
+                                                                        </AlertDialogTitle>
+                                                                        <AlertDialogDescription>
+                                                                            This action cannot be undone. Thiw will permanently delete selected customer from the database.
+                                                                        </AlertDialogDescription>
+                                                                    </AlertDialogHeader>
+                                                                    <AlertDialogFooter>
+                                                                        <AlertDialogCancel>
+                                                                            Cancel
+                                                                        </AlertDialogCancel>
+                                                                        <AlertDialogAction onClick={() => handleCustomerDelete(customer.customer_id)}>
+                                                                            Delete
+                                                                        </AlertDialogAction>
+                                                                    </AlertDialogFooter>
+                                                                </AlertDialogContent>
+                                                            </AlertDialog>
+                                                        </div>
+                                                    </div>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            )}
+                        </>
+            }
+        </div>
     )
 }
 
