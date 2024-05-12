d14e92096bb8ca89ec6d8e87618edb19cde5c81e
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-12 10:48:06 +0300

    Vehicles table rendered

--- src/app/components/fleet/page.tsx
+++ src/app/components/fleet/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+import VehiclesTable from "@/components/VehiclesTable";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
 const FleetPage = () => {
+    const router = useRouter();
+
     return (
-        <section className="container flex flex-col items-center justify-center min-h-screen">
-            MyFleet component page
+        <section className="container flex flex-col items-center justify-center min-h-screen space-y-11">
+            <h1 className="text-3xl font-bold mt-10">Vehicles Table</h1>
+            <VehiclesTable />
+            <Button
+                className="py-8 shadow-2xl font-bold text-md"
+                onClick={() => router.push('customers/new-customer')}>
+                Add a New Vehicle
+            </Button>
         </section>
     )
 }
