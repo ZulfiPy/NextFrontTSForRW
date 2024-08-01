93f13d13d2ac677ae724647b7cc89b458cd5db35
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-08-01 09:13:36 +0300

    next auth type deleted

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,19 +1,3 @@
-import { DefaultSession } from "next-auth";
-
-declare module "next-auth" {
-    interface User {
-        id?: string;
-        firstName?: string,
-        lastName?: string,
-        username?: string;
-        roles?: string;
-    }
-
-    interface Session {
-        user: User & DefaultSession["user"]
-    }
-}
-
 export type RegisterUserData = {
     firstName: string,
     lastName: string,
