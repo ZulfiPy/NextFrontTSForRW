90ccae066fb67df619a5475a059eca5e160d1f7b
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-08-01 10:51:12 +0300

    register page modified with conditional rendering logic

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,17 @@
 import RegisterForm from "@/components/RegisterForm";
+import { validateRequest } from "@/lib/auth";
 
-const RegisterPage = () => {
+const RegisterPage = async () => {
+    const { session } = await validateRequest();
+
     return (
         <section className="container flex flex-col items-center justify-center min-h-screen">
-            <RegisterForm />
+            {!session ?
+                <RegisterForm /> :
+                <p>
+                    You are signed in, this page is only available for non-authenticated users!
+                </p>
+            }
         </section>
     )
 }
