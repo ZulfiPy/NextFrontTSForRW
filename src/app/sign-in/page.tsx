dbc7b06dc2aba71e7ad48c54a7f1154b6c536b04
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-08-01 10:51:37 +0300

    sign-in page modified with conditional rendering logic

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,9 +1,24 @@
 import SignInForm from "@/components/SignInForm";
+import { validateRequest } from "@/lib/auth";
+import Link from "next/link";
 
-const SignInPage = () => {
+const SignInPage = async () => {
+    const { session } = await validateRequest();
+
     return (
         <section className="container flex flex-col items-center justify-center min-h-screen">
-            <SignInForm />
+            {!session ?
+                <SignInForm /> :
+                <>
+                    <p>You are signed in.</p>
+                    <Link href='components'
+                        className="font-bold bg-blue-700 p-2 rounded-lg mt-2 hover:underline"
+                    >
+                        Check the Components
+                    </Link>
+                </>
+            }
+
         </section>
     )
 }
